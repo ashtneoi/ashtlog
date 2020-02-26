13e20de77ed3ad814948af884bebb6a49de34333
ashtneoi <ashtneoi@gmail.com>
2020-02-26 10:21:04 -0800

    Add simple python attempt

---
+++ python/ashtlog/__init__.py
@@ -0,0 +1,87 @@
+import sys
+
+
+class LogReceiver:
+    def receive(self, entry, node):
+        raise Exception("unimplemented")
+
+
+class LogNode:
+    locked = False
+
+    def __init__(self, receiver, parent=None, name=None):
+        self.receiver = receiver
+        self.parent = parent
+        self.name = name
+
+    def __enter__(self):
+        if self.parent:
+            self.parent.locked = True
+        return self
+
+    def __exit__(self, _exc_type, _exc_val, _exc_tb):
+        if self.parent:
+            self.parent.locked = False
+
+    def put(self, entry):
+        if self.locked:
+            raise Exception("locked")
+        self.receiver.receive(entry, self)
+
+    def child(self, entry):
+        if self.locked:
+            raise Exception("locked")
+        self.put(entry)
+        return LogNode(self.receiver, parent=self)
+
+    def child_shared(self, name):
+        if self.locked:
+            raise Exception("locked")
+        return LogNode(self.receiver, parent=self, name=name)
+
+
+# TODO: Implement this in other reasonable ways and compare them.
+class Standard(LogReceiver):
+    def __init__(self, target):
+        self.target = target
+
+    def receive(self, entry, node):
+        indentation = -1
+        n = node
+        nodes = []  # leaf to root
+        while n:
+            indentation += 1
+            nodes.append(n)
+            n = n.parent
+
+        self.target.write('  ' * indentation)
+        self.target.write('[')
+        for n in reversed(nodes):  # root to leaf
+            if n.parent and n.parent.name and n.name:
+                self.target.write('/')
+            if n.name:
+                self.target.write(n.name)  # FIXME: Escape '/' and '>'.
+            else:
+                self.target.write('>')
+        self.target.write('] ')
+        self.target.write(entry)
+        self.target.write('\n')
+
+
+if __name__ == '__main__':
+    rec = Standard(sys.stdout)
+    log = LogNode(rec)
+    log.put("hi")
+    with log.child("child 1") as log_child1:
+        log_child1.put("okay")
+        log_child1.put("yeah")
+    with log.child("child 2") as log_child2:
+        log_child2.put("idk")
+    out = log.child_shared('stdout')
+    err = log.child_shared('stderr')
+    out.put("everything is fine")
+    out_more = out.child_shared("doing-stuff")
+    out_more.put("okay doing stuff")
+    err.put("oh no")
+    out.put("it's fine")
+    log.put("bye")
---
+++ python/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name="ashtlog",
+        version="0.0.1",
+        author="ashtneoi",
+        author_email="ashtneoi@gmail.com",
+        description="Logging stuff",
+        url="https://github.com/ashtneoi/ashtlog",
+        packages=setuptools.find_packages(),
+        python_requires='>=3.6',
+    )

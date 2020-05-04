72d240b3b7b87fccb1fb66bf757558b8a77a35f2
ashtneoi <ashtneoi@gmail.com>
2020-05-03 17:05:01 -0700

    Add Python package skeleton

---
+++ python/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+__version__ = "0.0.0"
+
+
+if __name__ == "__main__":
+    setup(
+        name="ashtlog",
+        python_requires=">=3.6",
+        version=__version__,
+        package=("ashtlog",),
+    )

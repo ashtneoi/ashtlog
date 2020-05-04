d2c7aa606ffc26232df2a81dd348dbc74e4aee24
ashtneoi <ashtneoi@gmail.com>
2020-05-03 18:52:03 -0700

    Basic Python implementation

---
+++ python/ashtlog/__init__.py
@@ -0,0 +1,64 @@
+def escape(x, escaped, escape_char):
+    xx = []
+    for c in x:
+        if c in escaped:
+           xx.append(escape_char + c)
+        else:
+            xx.append(c)
+    return "".join(xx)
+
+
+class LogReceiver:
+    def put(self, entry):
+        pass
+
+    def child(self, entry):
+        pass
+
+    def child_shared(self, name):
+        pass
+
+
+class NullLogReceiver(LogReceiver):
+    def put(self, entry):
+        pass
+
+    def child(self, entry):
+        pass
+
+    def child_shared(self, name):
+        pass
+
+
+class PlainLogReceiver(LogReceiver):
+    def __init__(self, parent=None, name=None):
+        self.parent = parent
+        self.name = name
+        super().__init__()
+
+    def _get_path(self):
+        segments = []
+        here = self
+        while here is not None:
+            if here.name is None:
+                segments.append(">")
+            else:
+                segments.append(escape(here.name, "/>\\", "\\"))
+            here = here.parent
+        segments.reverse()
+        for i in range(1, len(segments)):
+            if segments[i - 1] != ">":
+                segments[i] = "/" + segments[i]
+
+        return "".join(segments), len(segments)
+
+    def put(self, entry):
+        path, indent = self._get_path()
+        print("  " * indent + path + " " + entry)
+
+    def child(self, entry):
+        self.put(entry)
+        return type(self)(self)
+
+    def child_shared(self, name):
+        return type(self)(self, name=name)
---
+++ python/x.py
@@ -0,0 +1,27 @@
+from ashtlog import PlainLogReceiver
+
+
+def main():
+    a = PlainLogReceiver()
+    b = a.child("b")
+    b.put("1")
+    c = b.child_shared("c")
+    c.put("2")
+    d = b.child_shared("d")
+    d.put("3")
+    e = a.child("e")
+    e.put("4")
+    f = e.child_shared("f")
+    f.put("5")
+    a.put("6")
+
+    g = a.child("g")
+    h = g.child_shared("h")
+    i = h.child_shared("i")
+    j = i.child("j")
+    k = j.child("k")
+    k.put("7")
+
+
+if __name__ == "__main__":
+    main()
